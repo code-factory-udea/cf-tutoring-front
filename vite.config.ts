83f70394cfe41c04f9d838182e3487f634fb9ed9
Eliana Puerta <80785262+Eliana-Janneth@users.noreply.github.com>
2024-08-15 20:57:45 -0500

    feat: Add axios configuration and user validation hook

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import * as path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@axios": path.resolve(__dirname, './src/axios'),
+      "@components": path.resolve(__dirname, './src/components'),
+      "@services": path.resolve(__dirname, './src/services'),
+      "@hooks": path.resolve(__dirname, './src/hooks'),
+      "@utils": path.resolve(__dirname, './src/utils'),
+      "@assets": path.resolve(__dirname, './src/assets'),
+      "@interfaces": path.resolve(__dirname, './src/interfaces'),
+      "@pages": path.resolve(__dirname, './src/pages'),
+    },
+  },
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+});
+
