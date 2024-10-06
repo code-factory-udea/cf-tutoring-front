4923b99d64a94ae6f134512a4c14f8b02f865bac
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2024-10-06 15:47:25 -0500

    fix: deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -20,10 +20,10 @@ export default defineConfig({
   },
   server: {
     https: {
-      key: 'codefact-2024.key',
-      cert: 'codefact-2024.crt',
+      key: './codefact-2024.key',
+      cert: './codefact-2024.crt',
     },
-    host: "0.0.0.0",
+    host: "localhost",
     port: 3000,
   },
 });
