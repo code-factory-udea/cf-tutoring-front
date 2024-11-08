43412a7bf213fed9e5a96be9d1d88717eb28a42c
mateo.velasquez <mateo.velasquez@ServiciosNutresa.onmicrosoft.com>
2024-11-08 11:08:48 -0500

    feat: changes in dates, and pages

--- src/pages/requestConfirmedOrCancel.page.tsx
+++ src/pages/requestConfirmedOrCancel.page.tsx
@@ -1,5 +1,44 @@
-const RequestConfirmedOrCancelPage = () => {
-    return ("Vista de solicitudes Confirmadas y cancelacion de citas")
-}
+import { useQueryPendingAppointments } from "@hooks/queries";
+import { Table } from "@ui/Table";
+import { APPOINMENT_STATUS } from "@utils/constants";
+import moment from "moment";
+import { useMemo } from "react";
+import { ToastContainer } from "react-toastify";
+
+const COLUMNS = ["ID", "Nombre", "Fecha", "Virtual"];
+export const RequestConfirmedOrCancelPage = () => {
+  const { data: confirmedRequests, isLoading: isLoadingConfirmed } = useQueryPendingAppointments(APPOINMENT_STATUS.ACCEPTED);
+
+  const memoizedRequest = useMemo(() => {
+    if (!confirmedRequests) return [];
+    const requests = confirmedRequests;
+    requests.sort((a, b) => moment(a.date).diff(moment(b.date)));
+
+    return requests.map((request) => {
+      return [
+        <p>{request.id}</p>,
+        <p>{request.name}</p>,
+        <p>{moment(request.date).format("YYYY/MM/DD")}, {request.startTime} - {request.endTime}</p>,
+        <p>{request.virtual ? "Si" : "No"}</p>,
+      ];
+    });
+  }, [confirmedRequests]);
+
+  return (
+    <div className="p-6 text-dark">
+      <h1 className="text-2xl font-bold mb-4">
+        Solicitudes Aceptadas
+      </h1>
+
+      <Table
+        columns={COLUMNS}
+        data={memoizedRequest}
+        isLoadingData={isLoadingConfirmed}
+      />
+
+      <ToastContainer />
+    </div>
+  );
+};
 
 export default RequestConfirmedOrCancelPage;
--- src/pages/requestRejected.page.tsx
+++ src/pages/requestRejected.page.tsx
@@ -1,5 +1,44 @@
-const RequestRejectedPage = () => {
-    return ("Vista de solicitudes rechazadas")
-}
+import { useQueryPendingAppointments } from "@hooks/queries";
+import { Table } from "@ui/Table";
+import { APPOINMENT_STATUS } from "@utils/constants";
+import moment from "moment";
+import { useMemo } from "react";
+import { ToastContainer } from "react-toastify";
+
+const COLUMNS = ["ID", "Nombre", "Fecha", "Virtual"];
+export const RequestRejectedPage = () => {
+  const { data: rejectedRequests, isLoading: isLoadingRejected } = useQueryPendingAppointments(APPOINMENT_STATUS.REJECTED);
+
+  const memoizedRequest = useMemo(() => {
+    if (!rejectedRequests) return [];
+    const requests = rejectedRequests;
+    requests.sort((a, b) => moment(a.date).diff(moment(b.date)));
+
+    return requests.map((request) => {
+      return [
+        <p>{request.id}</p>,
+        <p>{request.name}</p>,
+        <p>{moment(request.date).format("YYYY/MM/DD")}, {request.startTime} - {request.endTime}</p>,
+        <p>{request.virtual ? "Si" : "No"}</p>,
+      ];
+    });
+  }, [rejectedRequests]);
+
+  return (
+    <div className="p-6 text-dark">
+      <h1 className="text-2xl font-bold mb-4">
+        Solicitudes Rechazadas - Estudiantes
+      </h1>
+
+      <Table
+        columns={COLUMNS}
+        data={memoizedRequest}
+        isLoadingData={isLoadingRejected}
+      />
+
+      <ToastContainer />
+    </div>
+  );
+};
 
 export default RequestRejectedPage;
